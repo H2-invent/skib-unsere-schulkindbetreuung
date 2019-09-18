105c0ed2aaf248119f696931f73042fad6a7fdd3
Emanuel HOlzmann <eh@tunze.com>
2019-09-18 22:21:21 +0200

    start mitarbeiter

--- src/Controller/EmployeeController.php
+++ src/Controller/EmployeeController.php
@@ -2,18 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\Stadt;
+use App\Entity\User;
+use FOS\UserBundle\Model\UserManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EmployeeController extends AbstractController
 {
-    /**
-     * @Route("/employee", name="employee")
-     */
-    public function index()
+    private $manager;
+
+    public function __construct(UserManagerInterface $manager)
     {
-        return $this->render('employee/index.html.twig', [
-            'controller_name' => 'EmployeeController',
+        $this->manager = $manager;
+    }
+
+    /**
+     * @Route("/city/admin/mitarbeiter/stadt", name="employee")
+     */
+    public function index(Request $request)
+    {
+        $city= $this->getDoctrine()->getRepository(Stadt::class)->find($request->get('id'));
+        $user =
+        $user = $this->getDoctrine()->getRepository(User::class)->findBy(array('stadt'=>$city));
+        dump($user);
+        return $this->render('employee/user.html.twig', [
+            'user' => $user,
+            'city'=>$city
         ]);
     }
 }
