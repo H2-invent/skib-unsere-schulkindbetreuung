d493c7956f4a320239a3945597ce9127d250f953
andreas.holzmann <andreas.holzmann@capgemini.com>
2019-10-11 14:56:15 +0200

    start the landingpage for the main page // Changed Ganztagsbetreuung into schulkindbetreuung fpr the Loerrach page

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Stadt;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractController
@@ -10,9 +12,11 @@ class DashboardController extends AbstractController
     /**
      * @Route("/", name="index")
      */
-    public function index()
+    public function index(Request $request)
     {
-        return $this->redirectToRoute('dashboard');
+        $stadt = array();
+
+        return $this->render('landing/start.html.twig');
     }
     /**
      * @Route("/login/dashboard", name="dashboard")
