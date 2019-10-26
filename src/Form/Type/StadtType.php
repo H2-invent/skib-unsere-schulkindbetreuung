43531503e0491877d6455546c2f0c7f32abe4ea6
andreas.holzmann <andreas.holzmann@capgemini.com>
2019-10-27 00:02:05 +0200

    add infotext and agb in stadtverwaltung

--- src/Form/Type/StadtType.php
+++ src/Form/Type/StadtType.php
@@ -48,8 +48,14 @@ class StadtType extends AbstractType
             ->add('hauptfarbe', TextType::class,['required'=>false,'label'=>'Hauptfarbe(HTML Code)','translation_domain' => 'form'])
             ->add('akzentfarbe', TextType::class,['required'=>false,'label'=>'Akzentfarbe (HTML Code)','translation_domain' => 'form'])
             ->add('akzentfarbeFehler', TextType::class,['required'=>false,'label'=>'Akzentfarbe Fehler (HTML Code)','translation_domain' => 'form'])
-
+            ->add('infoTextDe', TextareaType::class, ['required'=>false,'label'=>'Info Text Deutsch','translation_domain' => 'form', 'property_path' => 'translations[de].infoText', ])
+            ->add('infoTextEn', TextareaType::class, ['required'=>false,'label'=>'Info Text Englisch','translation_domain' => 'form','property_path' => 'translations[en].infoText', ])
+            ->add('infoTextFr', TextareaType::class, ['required'=>false,'label'=>'Info Text Französisch','translation_domain' => 'form','property_path' => 'translations[fr].infoText', ])
+            ->add('infoAGBDe', TextareaType::class, ['required'=>false,'label'=>'AGB Deutsch','translation_domain' => 'form', 'property_path' => 'translations[de].agb', ])
+            ->add('infoAGBEn', TextareaType::class, ['required'=>false,'label'=>'AGB Englisch','translation_domain' => 'form','property_path' => 'translations[en].agb', ])
+            ->add('infoAGBFr', TextareaType::class, ['required'=>false,'label'=>'AGB Französisch','translation_domain' => 'form','property_path' => 'translations[fr].agb', ])
             ->add('submit', SubmitType::class, ['label' => 'Speichern','translation_domain' => 'form'])
+
         ;
     }
     public function configureOptions(OptionsResolver $resolver)
