e6085bc6b08d1870ae1ca54d90ff0c18db88b564
Emanuel Holzmann <emanuel-holzmann@web.de>
2019-10-29 20:58:50 +0100

    start with the dashboard

---
+++ src/Controller/WidgetController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Active;
+use App\Entity\Kind;
+use App\Entity\Organisation;
+use App\Entity\Zeitblock;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\DateTime;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class WidgetController extends AbstractController
+{
+    /**
+     * @Route("/org_child/show/widget/kidsToday", name="widget_kids_today")
+     */
+    public function index(Request $request,TranslatorInterface $translator)
+    {
+        $organisation = $this->getDoctrine()->getRepository(Organisation::class)->find($request->get('id'));
+        if ($organisation != $this->getUser()->getOrganisation()) {
+            throw new \Exception('Wrong Organisation');
+        }
+        $today = (new \DateTime())->format('w');
+        if($today== 0){
+            $today = 6;
+        }else{
+            $today = $today-1;
+        }
+
+        $stadt = $this->getUser()->getStadt();
+        $active = $this->getDoctrine()->getRepository(Active::class)->findActiveSchuljahrFromCity($stadt);
+         $qb = $this->getDoctrine()->getRepository(Kind::class)->createQueryBuilder('k')
+            ->innerJoin('k.zeitblocks','b')
+            ->andWhere('b.wochentag = :wochentag')
+             ->andWhere('b.active = :jahr');
+             foreach ($organisation->getSchule() as $key=>$data){
+                 $qb->orWhere('b.schule = :schule'.$key)
+                     ->setParameter('schule'.$key,$data);
+             };
+             $qb->andWhere('k.fin = true');
+            $qb->setParameter('jahr',$active)
+                ->setParameter('wochentag',$today);
+        $query = $qb->getQuery();
+        $kinder = $result = $query->getResult();
+        dump($kinder);
+
+        return new JsonResponse(array('title'=>$translator->trans('Anwesende Kinder heute'),'anzahl'=>sizeof($kinder),'symbol'=>'emoji_people'));
+    }
+
+    /**
+     * @Route("/org_child/show/widget/kidsSchuljahr", name="widget_kids_schuljahr")
+     */
+    public function schuljahr(Request $request,TranslatorInterface $translator)
+    {
+        $organisation = $this->getDoctrine()->getRepository(Organisation::class)->find($request->get('id'));
+        if ($organisation != $this->getUser()->getOrganisation()) {
+            throw new \Exception('Wrong Organisation');
+        }
+        $today = (new \DateTime())->format('w');
+        if($today== 0){
+            $today = 6;
+        }else{
+            $today = $today-1;
+        }
+
+        $stadt = $this->getUser()->getStadt();
+        $active = $this->getDoctrine()->getRepository(Active::class)->findActiveSchuljahrFromCity($stadt);
+        $qb = $this->getDoctrine()->getRepository(Kind::class)->createQueryBuilder('k')
+            ->innerJoin('k.zeitblocks','b')
+
+            ->andWhere('b.active = :jahr');
+        foreach ($organisation->getSchule() as $key=>$data){
+            $qb->orWhere('b.schule = :schule'.$key)
+                ->setParameter('schule'.$key,$data);
+        };
+        $qb->andWhere('k.fin = true');
+        $qb->setParameter('jahr',$active);
+
+        $query = $qb->getQuery();
+        $kinder = $result = $query->getResult();
+        dump($kinder);
+
+        return new JsonResponse(array('title'=>$translator->trans('Kinder dieses Schuljahr'),'anzahl'=>sizeof($kinder),'symbol'=>'emoji_people'));
+    }
+}
