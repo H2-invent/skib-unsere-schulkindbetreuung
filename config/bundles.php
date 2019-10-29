5b3e2d2ae56f2236a0e0b19b42e3730d30e9f4b3
Emanuel Holzmann <emanuel-holzmann@web.de>
2019-10-29 18:21:39 +0100

    change the uploader to the flysystem

--- config/bundles.php
+++ config/bundles.php
@@ -17,12 +17,10 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
     Shivas\VersioningBundle\ShivasVersioningBundle::class => ['all' => true],
-    League\FlysystemBundle\FlysystemBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Beelab\Recaptcha2Bundle\BeelabRecaptcha2Bundle::class => ['all' => true],
     JMS\I18nRoutingBundle\JMSI18nRoutingBundle::class => ['all' => true],
     WhiteOctober\TCPDFBundle\WhiteOctoberTCPDFBundle::class => ['all' => true],
-    Oneup\FlysystemBundle\OneupFlysystemBundle::class => ['all' => true],
     Cron\CronBundle\CronCronBundle::class => ['all' => true],
-
+    Oneup\FlysystemBundle\OneupFlysystemBundle::class => ['all' => true],
 ];
