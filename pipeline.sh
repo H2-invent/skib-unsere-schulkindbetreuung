1c7a256416de146cdf9e0cf6a9378ae6969362c5
Emanuel Holzmann <emanuel-holzmann@web.de>
2019-10-30 13:32:34 +0000

    pipeline.sh edited online with Bitbucket

--- pipeline.sh
+++ pipeline.sh
@@ -3,7 +3,14 @@ echo ---------------Git pull--------------------------------------------------
 echo --------------------------------------------------------------------------
 git add .
 git stash
-git pull
+git fetch --tags
+
+# Get latest tag name
+latestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
+echo Neueste Version: $latestTag
+# Checkout latest tag
+git checkout $latestTag
+
 echo ------------Update parameter.yml and install latest packages-------------
 php composer.phar install
 echo --------------------------------------------------------------------------
