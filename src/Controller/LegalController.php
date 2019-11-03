bffbc7ad834b56af8fa49bba1440001f25270121
andreas.holzmann <andreas.holzmann@capgemini.com>
2019-11-03 21:20:41 +0100

    changed privcy page and footer

--- src/Controller/LegalController.php
+++ src/Controller/LegalController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Organisation;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,10 +11,9 @@ class LegalController extends AbstractController
     /**
      * @Route("/privacy", name="datenschutz")
      */
-    public function datenschutzAction()
+    public function privacyAction()
     {
-
-        return $this->render('legal/datenschutz.html.twig');
+            return $this->render('legal/datenschutz.html.twig');
     }
 
     /**
