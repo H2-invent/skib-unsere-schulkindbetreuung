08a40b6a6143b87335b7d357604c54bda95e4064
Emanuel Holzmann <emanuel-holzmann@web.de>
2019-11-06 21:41:23 +0100

    now with clickheat

--- src/Controller/BerichtController.php
+++ src/Controller/BerichtController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Active;
+use App\Entity\Kind;
+use App\Entity\Schule;
+use App\Entity\Stadt;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BerichtController extends AbstractController
@@ -10,10 +15,35 @@ class BerichtController extends AbstractController
     /**
      * @Route("/city_report/index", name="stadt_bericht_index")
      */
-    public function index()
+    public function index(Request $request)
     {
-        return $this->render('bericht/index.html.twig', [
-            'controller_name' => 'BerichtController',
-        ]);
+        $stadt = $this->getDoctrine()->getRepository(Stadt::class)->find($request->get('stadt_id'));
+        if ($stadt != $this->getUser()->getStadt()) {
+            throw new \Exception('Wrong City');
+        }
+
+        $qb = $this->getDoctrine()->getRepository(Kind::class)->createQueryBuilder('k')
+            ->innerJoin('k.zeitblocks','b');
+
+        //$qb = $repo->createQueryBuilder('b');
+         $blocks = array();
+
+            foreach ($stadt->getSchules() as $key=>$data){
+                $qb->orWhere('b.schule = :schule'.$key)
+                    ->setParameter('schule'.$key,$data);
+            }
+
+        if($request->get('schuljahr')){
+            $jahr = $this->getDoctrine()->getRepository(Active::class)->find($request->get('schuljahr'));
+            $qb->andWhere('b.active = :jahr')
+                ->setParameter('jahr',$jahr);
+        }
+
+        $qb->andWhere('k.fin = 1');
+        $query = $qb->getQuery();
+        $blocks = $result = $query->getResult();
+        $kinder = $blocks;
+        dump($blocks);
+        return $this->render('bericht/index.html.twig',array('kinder'=>$kinder));
     }
 }
