c89990ed944c8903d5d93f50ded9551570848b05
Emanuel Holzmann <emanuel-holzmann@web.de>
2019-11-06 14:45:08 +0100

    try phpunit and pipeline

---
+++ tests/DashboardTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class DashboardTest extends TestCase
+{
+    public function testSomething()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/WorkflowTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class WorkflowTest extends TestCase
+{
+    public function testSomething()
+    {
+        $this->assertTrue(true);
+    }
+}
