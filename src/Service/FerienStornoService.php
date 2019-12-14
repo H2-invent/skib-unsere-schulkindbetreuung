e1009dcd9ebc011cbbe06c92603ce00b0864344f
Emanuel Holzmann <emanuel-holzmann@web.de>
2019-12-14 17:25:31 +0100

    clean up

--- src/Service/FerienStornoService.php
+++ src/Service/FerienStornoService.php
@@ -1,49 +1,12 @@
 <?php
 
 namespace App\Service;
-
-use App\Entity\Active;
-use App\Entity\Ferienblock;
-use App\Entity\Kind;
 use App\Entity\KindFerienblock;
-use App\Entity\Organisation;
-use App\Entity\Payment;
-use App\Entity\PaymentSepa;
-use App\Entity\Rechnung;
-use App\Entity\Sepa;
-use App\Entity\Stadt;
-
 use App\Entity\Stammdaten;
-
-use App\Entity\Zeitblock;
-use App\Form\Type\ConfirmType;
-use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\EntityManagerInterface;
-
-use phpDocumentor\Reflection\Types\Boolean;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
-
-use Symfony\Component\Form\Extension\Core\Type\HiddenType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-
-
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormFactoryInterface;
-use Symfony\Component\Form\FormTypeInterface;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\RouterInterface;
-use Symfony\Component\Security\Core\Security;
-
 use Symfony\Contracts\Translation\TranslatorInterface;
-use Twig\Environment;
 use function Doctrine\ORM\QueryBuilder;
 
-
-// <- Add this
 
 class FerienStornoService
 {
