814ea230a9d849b71e66b2613eb4b702189925dc
Emanuel Holzmann <emanuel-holzmann@web.de>
2020-01-01 23:18:28 +0100

    add the feedback

--- src/Controller/LoerrachWorkflowController.php
+++ src/Controller/LoerrachWorkflowController.php
@@ -425,8 +425,20 @@ class LoerrachWorkflowController extends AbstractController
         foreach ($kind as $data) {
             $anmeldeEmailService->sendEmail($data, $adresse, $stadt, $this->einkommensgruppen);
         }
-
-        $response = $this->render('workflow/abschluss.html.twig', array('kind' => $kind, 'eltern' => $adresse, 'stadt' => $stadt));
+        $default = array(
+            'text'=>''
+        );
+        $form = $this->createFormBuilder($default)
+            ->add('test',TextType::class)
+            ->add('submit',SubmitType::class)
+            ->getForm();
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // data is an array with "name", "email", and "message" keys
+            $default = $form->getData();
+            // do with default data whatever you want ...
+        }
+        $response = $this->render('workflow/abschluss.html.twig', array('form'=>$form->createView(),'kind' => $kind, 'eltern' => $adresse, 'stadt' => $stadt));
         $response->headers->clearCookie('UserID');
         $response->headers->clearCookie('SecID');
         $response->headers->clearCookie('KindID');
