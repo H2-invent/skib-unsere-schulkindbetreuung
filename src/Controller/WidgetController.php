5ce6cf53f183b9e7e4ec12e016c59fb198be5f99
Emanuel Holzmann <emanuel-holzmann@web.de>
2020-01-03 13:07:50 +0100

    add the shool filter

--- src/Controller/WidgetController.php
+++ src/Controller/WidgetController.php
@@ -21,86 +21,86 @@ class WidgetController extends AbstractController
     /**
      * @Route("/org_child/show/widget/kidsToday", name="widget_kids_today")
      */
-    public function index(Request $request,TranslatorInterface $translator)
+    public function index(Request $request, TranslatorInterface $translator)
     {
         $organisation = $this->getDoctrine()->getRepository(Organisation::class)->find($request->get('id'));
         if ($organisation != $this->getUser()->getOrganisation()) {
             throw new \Exception('Wrong Organisation');
         }
         $today = (new \DateTime())->format('w');
-        if($today== 0){
+        if ($today == 0) {
             $today = 6;
-        }else{
-            $today = $today-1;
-        }
-
-        $stadt = $this->getUser()->getStadt();
-        $active = $this->getDoctrine()->getRepository(Active::class)->findActiveSchuljahrFromCity($stadt);
-         $qb = $this->getDoctrine()->getRepository(Kind::class)->createQueryBuilder('k')
-            ->innerJoin('k.zeitblocks','b')
-            ->andWhere('b.wochentag = :wochentag')
-             ->andWhere('b.active = :jahr');
-            $schulen = $qb->expr()->orX();
-             foreach ($organisation->getSchule() as $key=>$data){
-
-                    $schulen->add('b.schule = :schule'.$key);
-                         $qb->setParameter('schule'.$key,$data);
-             };
-             $qb->andWhere($schulen);
-             $qb->andWhere('k.fin = true');
-
-
-            $qb->setParameter('jahr',$active)
-                ->setParameter('wochentag',$today);
-        $query = $qb->getQuery();
-        $kinder = $result = $query->getResult();
-
-
-        return new JsonResponse(array('title'=>$translator->trans('Anwesende Kinder heute'),'small'=>'','anzahl'=>sizeof($kinder),'symbol'=>'emoji_people'));
-    }
-
-    /**
-     * @Route("/org_child/show/widget/kidsSchuljahr", name="widget_kids_schuljahr")
-     */
-    public function schuljahr(Request $request,TranslatorInterface $translator)
-    {
-        $organisation = $this->getDoctrine()->getRepository(Organisation::class)->find($request->get('id'));
-        if ($organisation != $this->getUser()->getOrganisation()) {
-            throw new \Exception('Wrong Organisation');
-        }
-        $today = (new \DateTime())->format('w');
-        if($today== 0){
-            $today = 6;
-        }else{
-            $today = $today-1;
+        } else {
+            $today = $today - 1;
         }
 
         $stadt = $this->getUser()->getStadt();
         $active = $this->getDoctrine()->getRepository(Active::class)->findActiveSchuljahrFromCity($stadt);
         $qb = $this->getDoctrine()->getRepository(Kind::class)->createQueryBuilder('k')
-            ->innerJoin('k.zeitblocks','b')
-
+            ->innerJoin('k.zeitblocks', 'b')
+            ->andWhere('b.wochentag = :wochentag')
             ->andWhere('b.active = :jahr');
         $schulen = $qb->expr()->orX();
-        foreach ($organisation->getSchule() as $key=>$data){
+        foreach ($organisation->getSchule() as $key => $data) {
 
-            $schulen->add('b.schule = :schule'.$key);
-            $qb->setParameter('schule'.$key,$data);
+            $schulen->add('b.schule = :schule' . $key);
+            $qb->setParameter('schule' . $key, $data);
         };
         $qb->andWhere($schulen);
         $qb->andWhere('k.fin = true');
-        $qb->setParameter('jahr',$active);
+
+
+        $qb->setParameter('jahr', $active)
+            ->setParameter('wochentag', $today);
+        $query = $qb->getQuery();
+        $kinder = $result = $query->getResult();
+
+
+        return new JsonResponse(array('title' => $translator->trans('Anwesende Kinder heute'), 'small' => '', 'anzahl' => sizeof($kinder), 'symbol' => 'emoji_people'));
+    }
+
+    /**
+     * @Route("/org_child/show/widget/kidsSchuljahr", name="widget_kids_schuljahr")
+     */
+    public function schuljahr(Request $request, TranslatorInterface $translator)
+    {
+        $organisation = $this->getDoctrine()->getRepository(Organisation::class)->find($request->get('id'));
+        if ($organisation != $this->getUser()->getOrganisation()) {
+            throw new \Exception('Wrong Organisation');
+        }
+        $today = (new \DateTime())->format('w');
+        if ($today == 0) {
+            $today = 6;
+        } else {
+            $today = $today - 1;
+        }
+
+        $stadt = $this->getUser()->getStadt();
+        $active = $this->getDoctrine()->getRepository(Active::class)->findActiveSchuljahrFromCity($stadt);
+        $qb = $this->getDoctrine()->getRepository(Kind::class)->createQueryBuilder('k')
+            ->innerJoin('k.zeitblocks', 'b')
+            ->andWhere('b.active = :jahr');
+        $schulen = $qb->expr()->orX();
+        foreach ($organisation->getSchule() as $key => $data) {
+
+            $schulen->add('b.schule = :schule' . $key);
+            $qb->setParameter('schule' . $key, $data);
+        };
+        $qb->andWhere($schulen);
+        $qb->andWhere('k.fin = true');
+        $qb->setParameter('jahr', $active);
 
         $query = $qb->getQuery();
         $kinder = $result = $query->getResult();
 
 
-        return new JsonResponse(array('title'=>$translator->trans('Kinder dieses Schuljahr'),'small'=>'','anzahl'=>sizeof($kinder),'symbol'=>'emoji_people'));
+        return new JsonResponse(array('title' => $translator->trans('Kinder dieses Schuljahr'), 'small' => '', 'anzahl' => sizeof($kinder), 'symbol' => 'emoji_people'));
     }
+
     /**
      * @Route("/org_child/show/widget/kidsinSchule", name="widget_kids_schule")
      */
-    public function childsInSchule(Request $request,TranslatorInterface $translator)
+    public function childsInSchule(Request $request, TranslatorInterface $translator)
     {
         $organisation = $this->getDoctrine()->getRepository(Organisation::class)->find($request->get('org_id'));
         if ($organisation != $this->getUser()->getOrganisation()) {
@@ -109,26 +109,26 @@ class WidgetController extends AbstractController
         $today = (new \DateTime())->format('w');
         $stadt = $this->getUser()->getStadt();
         $active = $this->getDoctrine()->getRepository(Active::class)->findActiveSchuljahrFromCity($stadt);
-        $schule = $this->getDoctrine()->getRepository(Schule::class)->findOneBy(array('organisation'=>$organisation,'id'=>$request->get('schule_id')));
+        $schule = $this->getDoctrine()->getRepository(Schule::class)->findOneBy(array('organisation' => $organisation, 'id' => $request->get('schule_id')));
         $qb = $this->getDoctrine()->getRepository(Kind::class)->createQueryBuilder('k')
-            ->innerJoin('k.zeitblocks','b')
+            ->innerJoin('k.zeitblocks', 'b')
             ->andWhere('b.active = :jahr')
             ->andWhere('b.schule = :schule')
-            ->setParameter('schule',$schule);
+            ->setParameter('schule', $schule);
 
         $qb->andWhere('k.fin = true');
-        $qb->setParameter('jahr',$active);
+        $qb->setParameter('jahr', $active);
 
         $query = $qb->getQuery();
         $kinder = $result = $query->getResult();
-        return new JsonResponse(array('title'=>$schule->getName(),'small'=>$translator->trans('Kinder angemeldet'),'anzahl'=>sizeof($kinder),'symbol'=>'sports_handball'));
+        return new JsonResponse(array('title' => $schule->getName(), 'small' => $translator->trans('Kinder angemeldet'), 'anzahl' => sizeof($kinder), 'symbol' => 'sports_handball'));
 
     }
 
     /**
      * @Route("/org_child/show/widget/stundenplan", name="widget_kids_stundenplan")
      */
-    public function blockansicht(Request $request,TranslatorInterface $translator)
+    public function blockansicht(Request $request, TranslatorInterface $translator)
     {
         $organisation = $this->getDoctrine()->getRepository(Organisation::class)->find($request->get('org_id'));
         if ($organisation != $this->getUser()->getOrganisation()) {
@@ -139,30 +139,31 @@ class WidgetController extends AbstractController
         $active = $this->getDoctrine()->getRepository(Active::class)->findActiveSchuljahrFromCity($stadt);
         $qb = $this->getDoctrine()->getRepository(Zeitblock::class)->createQueryBuilder('b')
             ->andWhere('b.active = :jahr')
-        ->andWhere('b.deleted = false');
-        $schulen = $qb->expr()->orX();
-        foreach ($organisation->getSchule() as $key=>$data){
-            $schulen->add('b.schule = :schule'.$key);
-            $qb->setParameter('schule'.$key,$data);
-        };
-        $qb->andWhere($schulen);
-        $qb->setParameter('jahr',$active);
-
-        $query = $qb->getQuery();
-        $blocks = $result = $query->getResult();
+            ->andWhere('b.deleted = false')
+            ->innerJoin('b.schule', 'schule')
+            ->andWhere('schule.organisation =:org')
+            ->setParameter('org', $organisation)
+            ->setParameter('jahr', $active);
+            if($request->get('schule_id')){
+                $schule = $this->getDoctrine()->getRepository(Schule::class)->find($request->get('schule_id'));
+                $qb->andWhere('b.schule =:schule')
+                    ->setParameter('schule',$schule);
+            }
+        $blocks = $qb->getQuery()->getResult();
 
         $blocksRender = array();
-        foreach ($blocks as $data){
-            $blocksRender[$data->getWochentag()][]= $data;
+        foreach ($blocks as $data) {
+            $blocksRender[$data->getWochentag()][] = $data;
         }
-
-        return $this->render('widget/blockContent.html.twig',array('blocks'=>$blocksRender));
+        $schule = $this->getDoctrine()->getRepository(Schule::class)->findBy(array('organisation'=>$organisation));
+        return $this->render('widget/blockContent.html.twig', array('org'=>$organisation,'blocks' => $blocksRender,'schule'=>$schule));
 
     }
+
     /**
      * @Route("/org_accounting/widget/overdueSepa", name="widget_overdue_sepa")
      */
-    public function sepa(Request $request,TranslatorInterface $translator)
+    public function sepa(Request $request, TranslatorInterface $translator)
     {
         $organisation = $this->getDoctrine()->getRepository(Organisation::class)->find($request->get('org_id'));
         if ($organisation != $this->getUser()->getOrganisation()) {
@@ -170,19 +171,19 @@ class WidgetController extends AbstractController
         }
         $lastMonth = (new \DateTime())->modify('first day of last Month');
 
-        $qb =$this->getDoctrine()->getRepository(Sepa::class)->createQueryBuilder('s');
+        $qb = $this->getDoctrine()->getRepository(Sepa::class)->createQueryBuilder('s');
         $qb->andWhere('s.von <= :today')
             ->andWhere('s.bis >= :today')
             ->andWhere('s.organisation = :org')
-            ->setParameter('today',$lastMonth)
-            ->setParameter('org',$organisation);
+            ->setParameter('today', $lastMonth)
+            ->setParameter('org', $organisation);
         $query = $qb->getQuery();
         $sepa = $result = $query->getResult();
 
-        if(sizeof($sepa)==0){
-            return new JsonResponse(array('title'=>$translator->trans('Sepa-Lastschrift fällig'),'small'=>'','anzahl'=>1,'symbol'=>'money'));
-        }else{
+        if (sizeof($sepa) == 0) {
+            return new JsonResponse(array('title' => $translator->trans('Sepa-Lastschrift fällig'), 'small' => '', 'anzahl' => 1, 'symbol' => 'money'));
+        } else {
             return 0;
         }
-     }
+    }
 }
