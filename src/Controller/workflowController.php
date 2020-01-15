53e02242a5d388a9acda8f239ffaa650bcec2b6b
Emanuel Holzmann <emanuel-holzmann@web.de>
2020-01-15 20:54:38 +0100

    make better meta

--- src/Controller/workflowController.php
+++ src/Controller/workflowController.php
@@ -50,15 +50,15 @@ class workflowController extends AbstractController
         $cityInfoText = $stadt->translate()->getInfoText();
         // Load all schools from the city into the controller as $schulen
         $schule = $this->getDoctrine()->getRepository(Schule::class)->findBy(array('stadt' => $stadt, 'deleted' => false));
-        $title = $translator->trans('Schulkindbetreuung und Ferienbetreuung der Stadt').' '.$stadt->getName().' | '.$translator->trans(' Hier anmelden');
+        $title = $translator->trans('Schulkindbetreuung und Ferienbetreuung von ').' '.$stadt->getName().' | '.$translator->trans(' Hier anmelden');
         $text = $stadt->translate()->getInfoText();
         $array = explode('. ',$text);
         $text = $array[0];
         $count = 0;
         $metaDescription = '';
         foreach ($array as $data){
-            $count = strlen($data);
-            if($count< 150){
+            $count += strlen($data);
+            if($count <= 160){
                 $metaDescription.= $data.'. ';
             }else{
                 break;
