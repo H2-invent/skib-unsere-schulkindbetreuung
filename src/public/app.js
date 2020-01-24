7dd8e12de4bb363c926e3e759675fd68ccacf9d7
Emanuel Holzmann <emanuel-holzmann@web.de>
2020-01-24 17:35:41 +0100

    tray the new asset

---
+++ src/public/app.js
@@ -0,0 +1,22 @@
+var $ = require('jquery');
+require('bootstrap');
+require('chartjs-color');
+require('chartjs-color');
+require('chart.js');
+require('chartjs-color-string');
+require('datatables.net');
+require('daterangepicker');
+require('jquery-clockpicker');
+
+require('malihu-custom-scrollbar-plugin');
+require('jquery.cookie');
+require('jquery-validation');
+require('jquery-confirm');
+require('material-icons');
+require('moment');
+require('popper.js');
+require('@popperjs/core');
+require('snackbarjs');
+(function() {
+    $('body').html('Using jQuery');
+})();
