08d8ad28fa195f60fb841192d25b2eedc2675678
Emanuel Holzmann <emanuel-holzmann@web.de>
2020-01-30 20:57:49 +0100

    fix the daterange picker

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -10,18 +10,24 @@ import '../css/frontend.css';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 import $ from 'jquery';
-
 global.$ = global.jQuery = $;
+
 import Popper from 'popper.js';
 
 global.Popper = Popper;
+import moment from 'moment';
+global.moment =moment;
+
 import('bootstrap');
 import('./mdb');
 import('snackbarjs');
-import('moment');
+
 import ('morecontent-js/dist/jquery.morecontent');
 import('jquery-confirm');
 import('./jquery.bs.gdpr.cookies');
+import('jquery-clockpicker');
+import('daterangepicker');
+
 
 $(document).on('click', '.loadContent', function (e) {
     e.preventDefault();
@@ -102,7 +108,28 @@ $(window).on('load', function () {
         $(function () {
             $('[data-toggle="popover"]').popover()
         });
+// here is the daterangepicker stuff
+    $('input[type="date"]').daterangepicker({
+        "singleDatePicker": true,
+        autoUpdateInput: false,
+        locale: {
+            "format": "YYYY-MM-DD",
+            "separator": " - ",
+        }
+    }, function (chosen_date) {
+        $(this).val(chosen_date.format('YYYY-MM-DD'));
+    })
+        .on('apply.daterangepicker', function (ev, picker) {
+            //do something, like clearing an input
+            var ele = $(this);
+            ele.val(picker.startDate.format('YYYY-MM-DD'))
+        });
 
+// here is the clockpicker stuff
+    $('input[type="time"]').jqclockpicker({
+        autoclose: true,
+        donetext: "OK"
+    });
     $('body').bsgdprcookies(bssettings);
 
     $('#cookiesBtn').on('click', function () {
