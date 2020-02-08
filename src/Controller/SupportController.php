f5bea9d3e281086078b5ccfda9a9831340de9595
Emanuel Holzmann <emanuel-holzmann@web.de>
2020-02-08 13:51:42 +0100

    basic form finished

--- src/Controller/SupportController.php
+++ src/Controller/SupportController.php
@@ -2,13 +2,16 @@
 
 namespace App\Controller;
 
+use App\Form\Type\SupportForm;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use it\thecsea\osticket_php_client\OsticketPhpClient;
 use it\thecsea\osticket_php_client\OsticketPhpClientException;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class SupportController extends AbstractController
 {
@@ -17,26 +20,35 @@ class SupportController extends AbstractController
      */
     public function index(Request $request, ParameterBagInterface $parameterBag)
     {
-     // todo create form for support
+        // todo create form for support
+        $arr = array(
+            'name' => $this->getUser()->getVorname() . '-' . $this->getUser()->getNachname(),
+            'email' => $this->getUser()->getEmail(),
+            'phone' => '',
+            'subject' => 'Betreff',
+            'message' => 'Nachricht',
+            'datenschutz' => true,
+            'topicId' => '17',
+            'user' => $this->getUser()->getUsername(),
+        );
+        $form = $this->createForm(SupportForm::class, $arr);
+        $form->handleRequest($request);
 
+        $errors = array();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $arr = $form->getData();
+            dump($arr);
+            $support = new OsticketPhpClient($parameterBag->get('osTicketUrl'), $parameterBag->get('osTicketApi'));
+             try{
+            $support->request('api/tickets.json', $arr);
 
-        $support = new OsticketPhpClient($parameterBag->get('osTicketUrl'), $parameterBag->get('osTicketApi'));
-        try{
-            $arr = array(
-                'name'=>'test',
-                'email'=>'info@h2-invent.com',
-                'phone'=>'0912321',
-                'subject'=>'subject',
-                'message'=>'message',
-                'datenschutz'=>true,
-                'topicId'=>'17',
-                'user'=>'test',
-            );
-         $support->request('api/tickets.json',$arr);
-       }catch(OsticketPhpClientException $e){
-            print $e->getMessage();
+            return $this->redirectToRoute('dashboard',array('snack'=>"Es wurde erfolgreich ein Ticket angelegt"));
+             }catch(OsticketPhpClientException $e){
+                 return $this->redirectToRoute('dashboard',array('snack'=>"Fehler beim Anlegen des Tickets."));
+
+             }
         }
+        return $this->render('administrator/neuValidate.html.twig', array('errors' => $errors, 'form' => $form->createView(), 'title' => "Melden Sie hier ein Problem oder machen Sie einen Verbesserungsvorschlag"));
 
-       return 0;
     }
 }
---
+++ src/Form/Type/SupportForm.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Emanuel
+ * Date: 17.09.2019
+ * Time: 20:29
+ */
+
+namespace App\Form\Type;
+
+
+use App\Entity\Stammdaten;
+use Symfony\Component\Form\AbstractType;
+
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class SupportForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+
+        $builder
+            ->add('name',TextType::class,['required' => true,'disabled'=>true, 'label' => 'Name', 'translation_domain' => 'form'])
+            ->add('email',TextType::class,['required' => true,'disabled'=>true, 'label' => 'Email', 'translation_domain' => 'form'])
+            ->add('user',TextType::class,['required' => true,'disabled'=>true, 'label' => 'Username', 'translation_domain' => 'form'])
+            ->add('phone', TextType::class, ['required' => true, 'label' => 'Telefonnummer', 'translation_domain' => 'form'])
+            ->add('subject', TextType::class, ['required' => true, 'label' => 'Betreff', 'translation_domain' => 'form'])
+            ->add('message', TextareaType::class, ['attr'=>array('rows'=>10),'required' => true, 'label' => 'Nachricht', 'translation_domain' => 'form'])
+            ->add('submit', SubmitType::class, ['attr' => array('class' => 'btn btn-outline-primary'), 'label' => 'Speichern', 'translation_domain' => 'form']);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+
+        ]);
+
+    }
+}
