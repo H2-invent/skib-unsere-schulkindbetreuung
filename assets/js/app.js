d620bdb41d4c2728357e273ec79869bd3a7b64e4
Emanuel Holzmann <emanuel-holzmann@web.de>
2020-02-09 19:03:46 +0100

    add froala as editor

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,14 +25,33 @@ import('jquery.cookie');
 import('jquery-validation');
 import('jquery-confirm');
 import('material-design-icons');
-import  snackbar from 'snackbarjs';
+import snackbar from 'snackbarjs';
+
 import('daterangepicker');
 import('datatables.net');
 import 'datatables.net-dt';
 import niceScroll from 'jquery.nicescroll';
+// Import TinyMCE
+import tinymce from 'tinymce/tinymce';
 
+// A theme is also required
+import 'tinymce/themes/silver/theme';
 
-$(".side-navbar").niceScroll({cursorcolor:'#0058B0'});
+// Any plugins you want to use has to be imported
+import 'tinymce/plugins/paste';
+import 'tinymce/plugins/link';
+import 'tinymce/plugins/table';
+import 'tinymce/plugins/code';
+import FroalaEditor from 'froala-editor';
+import ('froala-editor/js/plugins/align.min');
+import ('froala-editor/js/plugins/paragraph_style.min');
+import ('froala-editor/js/plugins/link.min');
+import ('froala-editor/js/plugins/paragraph_style.min');
+import ('froala-editor/js/plugins/paragraph_format.min');
+import ('froala-editor/js/plugins/code_view.min');
+import ('froala-editor/js/plugins/colors.min');
+
+$(".side-navbar").niceScroll({cursorcolor: '#0058B0'});
 
 $('#toggle-btn').on('click', function (e) {
 
@@ -68,6 +87,89 @@ $(document).ready(function () {
     }
 })
 $(window).on('load', function () {
+
+
+// Load a plugin.
+
+
+// Initialize editor.7
+    FroalaEditor.DefineIcon('insert', {NAME: 'plus', SVG_KEY: 'add'});
+    FroalaEditor.RegisterCommand('insert', {
+        title: 'Insert HTML',
+        focus: true,
+        undo: true,
+        refreshAfterCallback: true,
+        callback: function () {
+            this.html.insert('<div class="row"><div class="col-lg-6"><p>6/12</p></div><div class="col-lg-6"><p>6/12</p></div></div>');
+        }
+    });
+    new FroalaEditor('textarea', {
+          //  toolbarButtons: ['fontFamily','more_text', '|', 'fontSize', '|', 'paragraphFormat', '|', 'bold', 'italic', 'underline', 'undo', 'redo', 'more_misc','|','insert'],
+
+        }
+    );
+
+
+    tinymce.init({
+        selector: 'text',
+        plugins: ['paste', 'link', 'table','code',''],
+        toolbar: ' undo redo|bootrapRow bsCollum | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image|code',
+        height: 500,
+        content_css : '/myLayout.css',
+        setup: function (editor) {
+
+            editor.ui.registry.addButton('bootrapRow', {
+                text: 'BS Row',
+                onAction: function (_) {
+                    editor.insertContent('<div class="row"><div class="col-lg-6"><p>6/12</p></div><div class="col-lg-6"><p>6/12</p></div></div>');
+                }
+            });
+            editor.ui.registry.addMenuButton('bsCollum', {
+                text: 'BS Col',
+                fetch: function (callback) {
+                    var item = [
+
+                        {
+                            type: 'menuitem',
+                            text: 'col-lg-' + 1,
+                            onAction: function () {
+                                editor.insertContent('<div class="col-lg-1"><p>Here is the col-1 Collum</p></div>');
+                            }
+                        },
+                        {
+                            type: 'menuitem',
+                            text: 'col-lg-6',
+                            onAction: function () {
+                                editor.insertContent('<div class="col-lg-6"><p>Here is the col-6 Collum</p></div>');
+                            }
+                        },
+                        {
+                            type: 'menuitem',
+                            text: 'col-lg-12',
+                            onAction: function () {
+                                editor.insertContent('<div class="col-lg-12"><p>Here is the col-12 Collum</p></div>');
+                            }
+                        }
+
+
+                    ];
+
+
+                    var items = [
+                        {
+                            type: 'menuitem',
+                            text: 'col-lg-1',
+                            onAction: function () {
+                                editor.insertContent('<div class="col-lg-1">You clicked menu item 1!</div>');
+                            }
+                        }
+
+                    ];
+                    callback(item);
+                }
+            });
+        }
+    });
 
     $('input[type="date"]').daterangepicker({
         "singleDatePicker": true,
@@ -110,7 +212,7 @@ $(window).on('load', function () {
     });
 
 
-    $(document).on('click', '.deleteBtn',function (e) {
+    $(document).on('click', '.deleteBtn', function (e) {
         e.preventDefault();
         var url = $(this).attr('href');
         var type = $(this).attr('type');
