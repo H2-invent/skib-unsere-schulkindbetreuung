b9b0329682177ef222745d5fece837892b69d6d7
Emanuel Holzmann <emanuel-holzmann@web.de>
2020-03-25 00:59:13 +0100

    repait the role switch

---
+++ src/Service/CheckinSchulkindservice.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Kind;
+use App\Entity\Organisation;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+
+// <- Add this
+
+class CheckinSchulkindservice
+{
+
+
+    private $em;
+    private $translator;
+
+    public function __construct(TranslatorInterface $translator,  EntityManagerInterface $entityManager)
+    {
+        $this->em = $entityManager;
+        $this->translator = $translator;
+    }
+
+    public
+    function checkin($checkinID, $tag,Organisation $organisation)
+    {
+        $tagDate = new \DateTime($tag);
+        $kind = $this->em->getRepository(Kind::class)->findOneBy(array('' => $checkinID));
+
+        $result['error'] = false;
+        $result['errorText'] = $this->translator->trans('Kind erfolgreich eingecheckt');
+        $result['checkinText'] = $this->translator->trans('Eingecheckt');
+        $result['vorname']= $kindFerienBlock->getKind()->getVorname();
+        $result['nachname']= $kindFerienBlock->getKind()->getNachname();
+        $result['kurs']= $kindFerienBlock->getFerienblock()->translate()->getTitel();
+        if ($kindFerienBlock === null) {
+            $result['error'] = true;
+            $result['errorText'] = $this->translator->trans('Ticket ist falsch oder ungültig');
+
+            return $result;
+        }
+
+        $startdateFerienblock = $kindFerienBlock->getFerienblock()->getStartDate();
+        $enddateFerienblock = $kindFerienBlock->getFerienblock()->getEndDate();
+
+        if ($tagDate < $startdateFerienblock || $tagDate > $enddateFerienblock) {
+            $result['error'] = true;
+            $result['errorText'] = $this->translator->trans('Dieses Ticket ist an einem anderen Tag gültig');
+        }
+
+        $result['checkinDate'] = $tag;
+        $status = $kindFerienBlock->getCheckinStatus() !== null ? $kindFerienBlock->getCheckinStatus() : array();
+
+        if (in_array($result['checkinDate'], $status)) {
+            $result['error'] = true;
+            $result['errorText'] = $this->translator->trans('Kind für den heutigen Tag bereits eingecheckt');
+        }
+
+        if ($kindFerienBlock->getState() >= 20) {
+            $result['error'] = true;
+            $result['errorText'] = $this->translator->trans('Das Ticket wurde Storniert und kann nicht eingcheckt werden');
+        }
+        if ($result['error'] === false) {
+            $status[] = $result['checkinDate'];
+            $kindFerienBlock->setCheckinStatus($status);
+
+            $this->em->persist($kindFerienBlock);
+            $this->em->flush();
+        }
+        return $result;
+    }
+
+}
