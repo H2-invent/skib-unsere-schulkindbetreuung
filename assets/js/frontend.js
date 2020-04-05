478ee88b7b934a3cd2bcb03064335abfa4a43079
Emanuel Holzmann <emanuel-holzmann@web.de>
2020-04-05 18:29:59 +0200

    add the toast to all pages with fehlersammler

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -31,9 +31,28 @@ jarallax(document.querySelectorAll('.jarallax'), {
 });
 
 $(document).ready(function () {
-    $(errors).each(function (i) {
-        toastr.error( errors[i]);
-    });
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": true,
+        "progressBar": true,
+        "positionClass": "md-toast-top-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": 300,
+        "hideDuration": 1000,
+        "timeOut": 5000,
+        "extendedTimeOut": 1000,
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    };
+    if(typeof errors !=='undefined'){
+        $(errors).each(function (i) {
+            toastr["error"](errors[i]);
+        });
+    }
 
 })
 
