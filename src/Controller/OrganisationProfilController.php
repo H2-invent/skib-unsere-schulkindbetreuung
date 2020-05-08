371d6aa16cfefde71502c20da0828eb7975d9693
andreas.holzmann <andreas.holzmann@h2-invent.com>
2020-05-08 13:55:34 +0200

    Include Org Profil Title and meta description.

--- src/Controller/OrganisationProfilController.php
+++ src/Controller/OrganisationProfilController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class OrganisationProfilController extends AbstractController
 {
@@ -14,11 +15,14 @@ class OrganisationProfilController extends AbstractController
      * @Route("/profil/{slug}", name="organisation_profil")
      * @ParamConverter("organisation", options={"mapping"={"slug"="slug"}})
      */
-    public function index(Request $request, Organisation $organisation)
+    public function index(Request $request, Organisation $organisation, TranslatorInterface $translator)
     {
+        $title = $translator->trans('Organisationsprofil');
         return $this->render('organisation_profil/index.html.twig', [
             'organisation' => $organisation,
             'stadt' => $organisation->getStadt(),
+            'metaDescription' => $organisation->getInfoText(),
+            'title' => $title.' | '.$organisation->getName(),
         ]);
     }
 }
