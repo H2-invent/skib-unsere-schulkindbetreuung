ade1be4e4ccd49d2f6c9a6482c0be9aba022fbd1
Emanuel Holzmann <emanuel-holzmann@web.de>
2020-05-17 10:29:30 +0200

    webhoock for mailgun to react to fails emails

--- src/Service/GroupORMService.php
+++ src/Service/GroupORMService.php
@@ -13,18 +13,21 @@ class GroupORMService
 {
     public function groupData($inArr){
         $res = array();
-        $last = $inArr[0]->getCreatedAt();
-        $first = $inArr[sizeof($inArr)-1]->getCreatedAt();
-        for($i = $first; $i <= $last; $i->modify('+1 day')){
-            $res[$i->format('Y-m-d')] = 0;
-        }
-        foreach ($inArr as $data){
-            if(isset($res[$data->getCreatedAt()->format('Y-m-d')])){
-                $res[$data->getCreatedAt()->format('Y-m-d')]++;
-            }else{
-                $res[$data->getCreatedAt()->format('Y-m-d')] = 1;
+        if(sizeof($inArr)>1){
+            $last = $inArr[0]->getCreatedAt();
+            $first = $inArr[sizeof($inArr)-1]->getCreatedAt();
+            for($i = $first; $i <= $last; $i->modify('+1 day')){
+                $res[$i->format('Y-m-d')] = 0;
+            }
+            foreach ($inArr as $data){
+                if(isset($res[$data->getCreatedAt()->format('Y-m-d')])){
+                    $res[$data->getCreatedAt()->format('Y-m-d')]++;
+                }else{
+                    $res[$data->getCreatedAt()->format('Y-m-d')] = 1;
+                }
             }
         }
+
         return $res;
 
     }
