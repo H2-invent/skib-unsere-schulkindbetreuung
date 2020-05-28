a6857989c51577457313d6e983eabb3a3e9ea288
Emanuel Holzmann <emanuel-holzmann@web.de>
2020-05-28 16:10:46 +0200

    finished the preisliste

--- src/Controller/PreislisteController.php
+++ src/Controller/PreislisteController.php
@@ -4,21 +4,13 @@ namespace App\Controller;
 
 use App\Entity\Schule;
 use App\Entity\Stadt;
-use App\Entity\Zeitblock;
-use App\Service\ErrorService;
 use App\Service\PreisListeService;
 use App\Service\SchuljahrService;
-use App\Service\SchulkindBetreuungAdresseService;
-use App\Service\StamdatenFromCookie;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 class PreislisteController extends AbstractController
 {
@@ -32,5 +24,6 @@ class PreislisteController extends AbstractController
         $gehaltIst = $request->get('gehalt', sizeof($stadt->getGehaltsklassen()) - 1);
         $artIst = $request->get('art', 1);
         return new Response($preisListeService->preisliste($stadt,$schule,$gehaltIst,$artIst));
+
     }
 }
