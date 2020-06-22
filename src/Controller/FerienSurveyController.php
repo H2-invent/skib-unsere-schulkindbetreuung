964d7b015a5569aa060be890685d93705c71f746
Emanuel Holzmann <emanuel-holzmann@web.de>
2020-06-22 22:06:55 +0200

    add the survey stuff. the surveys are now saved in the database

--- src/Controller/FerienSurveyController.php
+++ src/Controller/FerienSurveyController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Ferienblock;
 use App\Entity\Organisation;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
@@ -14,7 +15,7 @@ class FerienSurveyController extends AbstractController
 {
 
     /**
-     * @Route("/org_ferien/edit/question", name="ferien_management_question", methods={"GET","POST"})
+     * @Route("/org_ferien/edit/question", name="ferien_management_question", methods={"GET"})
      */
     public function ferienblockFragen(Request $request, ValidatorInterface $validator, TranslatorInterface $translator)
     {
@@ -23,11 +24,30 @@ class FerienSurveyController extends AbstractController
             throw new \Exception('Wrong Organisation');
         }
         $ferienblock = $this->getDoctrine()->getRepository(Ferienblock::class)->findOneBy(array('id' => $request->get('ferien_id'), 'organisation' => $organisation));
-        if (sizeof($ferienblock->getCustomQuestion()) != 5 || $ferienblock->getAmountCustomQuestion() === null) {
-            $ferienblock->setCustomQuestion(array_fill(0, 5, ''));
+
+
+        return $this->render('ferien_survey/index.html.twig',array('ferien'=>$ferienblock));
+    }
+    /**
+     * @Route("/org_ferien/edit/question/save", name="ferien_management_question_save", methods={"POST"})
+     */
+    public function ferienblockFragenSave(Request $request, ValidatorInterface $validator, TranslatorInterface $translator)
+    {
+        $organisation = $this->getDoctrine()->getRepository(Organisation::class)->find($request->get('org_id'));
+        if ($organisation != $this->getUser()->getOrganisation()) {
+            throw new \Exception('Wrong Organisation');
+        }
+        try {
+            $ferienblock = $this->getDoctrine()->getRepository(Ferienblock::class)->findOneBy(array('id' => $request->get('id'), 'organisation' => $organisation));
+            $ferienblock->setIndividualQuestions($request->get('survey'));
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($ferienblock);
+            $em->flush();
+            return new JsonResponse(array('error'=>false,'text'=>$translator->trans('Erfolgreich gespeichert')));
+        }catch (\Exception $e){
+            return new JsonResponse(array('error'=>true,'text'=>$translator->trans('Fehler. Bitte versuchen Sie es erneut.')));
         }
 
 
-        return $this->render('ferien_survey/index.html.twig');
     }
 }
