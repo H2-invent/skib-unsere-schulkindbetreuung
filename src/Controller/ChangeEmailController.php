6d92a1a0f1c22710f91e0d8af01773787b858822
andreas.holzmann <andreas.holzmann@h2-invent.com>
2020-12-14 13:21:01 +0100

    Create service and form type for resetting the email of stammdaten

--- src/Controller/ChangeEmailController.php
+++ src/Controller/ChangeEmailController.php
@@ -3,12 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Kind;
-use App\Entity\Log;
-use App\Entity\Stammdaten;
 use App\Service\AnmeldeEmailService;
+use App\Service\ChildEmailChangeService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
@@ -18,7 +15,7 @@ class ChangeEmailController extends AbstractController
     /**
      * @Route("/org_child/email_change/", name="org_child_email_change")
      */
-    public function index(TranslatorInterface $translator, Request $request, AnmeldeEmailService $anmeldeEmailService)
+    public function index(TranslatorInterface $translator, Request $request, AnmeldeEmailService $anmeldeEmailService, ChildEmailChangeService $childChangeEmailService)
     {
         $kind = $this->getDoctrine()->getRepository(Kind::class)->find($request->get('kind_id'));
 
@@ -28,52 +25,21 @@ class ChangeEmailController extends AbstractController
         }
 
 
-        $input = array('email' => $kind->getEltern()->getEmail(), 'emailDoubleInput' => '');
-
-        $form = $this->createFormBuilder($input)
-            ->add('email', TextType::class, ['label' => 'Neue Email Adresse eingeben', 'translation_domain' => 'form'])
-            ->add('emailDoubleInput', TextType::class, ['label' => 'Neue Email Adresse erneut eingeben', 'translation_domain' => 'form'])
-            ->add('submit', SubmitType::class, ['attr' => array('class' => 'btn btn-outline-primary'), 'label' => 'Speichern', 'translation_domain' => 'form'])
-            ->getForm();
-
+        $form = $childChangeEmailService->form($kind);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
 
             $input = $form->getData();
-
             if ($input['email'] !== $input['emailDoubleInput']) {
-                $text = $translator->trans('Email Adressen stimmen nicht überein.');
+                $text = $this->translator->trans('Email Adressen stimmen nicht überein.');
                 return $this->redirectToRoute('org_child_email_change', array('kind_id' => $kind->getId(), 'snack' => $text));
             }
 
-            $elternOne = $kind->getEltern();
-            $elternAll = $this->getDoctrine()->getRepository(Stammdaten::class)->findBy(array('tracing' => $elternOne->getTracing()));
-
-            $kinder = $this->getDoctrine()->getRepository(Kind::class)->findBy(array('tracing' => $kind->getTracing()));
-            $message = 'Email addresse changed from ' . $elternOne->getEmail() . ' to ' . $input['email'] . '; ' .
-                'id: ' . $elternOne->getId() . '; ' . 'Tracing: ' . $elternOne->getTracing();
-            $log = new Log();
-            $log->setUser($this->getUser()->getEmail());
-            $log->setDate(new \DateTime());
-            $log->setMessage($message);
-            $this->getDoctrine()->getManager()->persist($log);
-            foreach ($elternAll as $data) {
-                $data->setEmail($input['email']);
-                $data->setEmailDoubleInput($input['emailDoubleInput']);
-                $this->getDoctrine()->getManager()->persist($data);
-            }
-            $this->getDoctrine()->getManager()->flush();
-            foreach ($elternOne->getKinds() as $data2) {
-                $anmeldeEmailService->sendEmail($data2, $elternOne, $data2->getSchule()->getStadt(), $translator->trans('Ihre E-Mail Adresse wurde von einem Mitarbeiter der betreuenden Organisation geändert. Aus diesem Grund senden wir Ihnen die Buchungsbstätigung dieses Kindes nochmals zu:', [], $elternOne->getLanguage()));
-                $anmeldeEmailService->setBetreff($translator->trans('Ihre E-Mail Adresse wurde von einem Mitarbeiter der betreuenden Oranisation geändert.', [], $elternOne->getLanguage()));
-                $anmeldeEmailService->send($data2, $data2->getEltern());
-            }
-
+            $childChangeEmailService->changeEmail($kind, $input, $this->getUser());
 
             $text = $translator->trans('Email adresse geändert');
             return $this->redirectToRoute('child_show', array('id' => $this->getUser()->getOrganisation()->getId(), 'snack' => $text));
-
         }
         return $this->render('child_change/email.html.twig', array('form' => $form->createView()));
     }
---
+++ src/Service/ChildEmailChangeService.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Kind;
+use App\Entity\Log;
+
+use App\Entity\Stammdaten;
+use App\Entity\User;
+use App\Form\Type\ChildChangeEmailType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+
+// <- Add this
+
+class ChildEmailChangeService
+{
+    private $em;
+    private $translator;
+    private $email;
+    private $form;
+
+    public function __construct(TranslatorInterface $translator, EntityManagerInterface $entityManager, AnmeldeEmailService $anmeldeEmailService, FormFactoryInterface $formBuilder)
+    {
+        $this->em = $entityManager;
+        $this->translator = $translator;
+        $this->email = $anmeldeEmailService;
+        $this->form = $formBuilder;
+    }
+
+    public function form(Kind $kind)
+    {
+        $input = array('email' => $kind->getEltern()->getEmail(), 'emailDoubleInput' => '');
+
+        $form = $this->form->create(ChildChangeEmailType::class,$input);
+
+        return $form;
+    }
+
+
+    public function changeEmail(Kind $kind, $input, User $user)
+    {
+
+        $elternOne = $kind->getEltern();
+        $elternAll = $this->em->getRepository(Stammdaten::class)->findBy(array('tracing' => $elternOne->getTracing()));
+
+        $message = 'Email addresse changed from ' . $elternOne->getEmail() . ' to ' . $input['email'] . '; ' .
+            'stamm_id: ' . $elternOne->getId() . '; ' .
+            'fos_user_id: ' . $user->getId() . '; '.
+            'Tracing: ' . $elternOne->getTracing();
+        $log = new Log();
+        $log->setUser($user->getEmail());
+        $log->setDate(new \DateTime());
+        $log->setMessage($message);
+        $this->em->persist($log);
+        foreach ($elternAll as $data) {
+            $data->setEmail($input['email']);
+            $data->setEmailDoubleInput($input['emailDoubleInput']);
+            $this->em->persist($data);
+        }
+        $this->em->flush();
+
+        foreach ($elternOne->getKinds() as $data2) {
+            $this->email->sendEmail($data2, $elternOne, $data2->getSchule()->getStadt(), $this->translator->trans('Ihre E-Mail Adresse wurde von einem Mitarbeiter der betreuenden Organisation geändert. Aus diesem Grund senden wir Ihnen die Buchungsbstätigung dieses Kindes nochmals zu:', [], $elternOne->getLanguage()));
+            $this->email->setBetreff($this->translator->trans('Ihre E-Mail Adresse wurde von einem Mitarbeiter der betreuenden Oranisation geändert.', [], $elternOne->getLanguage()));
+            $this->email->send($data2, $data2->getEltern());
+        }
+
+        return true;
+
+    }
+}
