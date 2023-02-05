2f87901b0a9d445aaebbf77194b9d257a30742d1
Entwicklung <entwicklung@h2-invent.com>
2023-02-05 10:11:58 +0100

    * frontend: Löschen geht weider * frontend: Datapicker holt sich min und max von dem feld * Kinder bearbeiten bei einem Kind in der Zukunft wird das Startdatum richtig gesetztz * BEi Auswahl eines Schuljahrs wird auch das Von Datum gesetzt

--- src/Form/Type/LoerrachKind.php
+++ src/Form/Type/LoerrachKind.php
@@ -37,7 +37,7 @@ class LoerrachKind extends AbstractType
         $today = intval((new \DateTime())->format('Y'));
         $stadt = $options['data']->getSchule()->getStadt();
 
-        if ($options['schuljahr']) {
+        if (isset($options['schuljahr']) && $options['schuljahr']) {
             $builder->add('startDate', DateType::class, ['attr' => array('class' => 'pickadate', 'data-min' => $options['schuljahr']->getVon()->format('d.m.Y'), 'data-max' => $options['schuljahr']->getBis()->format('d.m.Y')), 'widget' => 'single_text', 'label' => 'Startdatum', 'translation_domain' => 'form']);
         }
         $builder->add('vorname', TextType::class, ['label' => 'Vorname', 'translation_domain' => 'form'])
