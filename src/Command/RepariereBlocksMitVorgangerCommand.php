3e131f220865aca1b59f7fe8c6c1299a3df06c68
Entwicklung <entwicklung@h2-invent.com>
2024-02-20 15:14:39 +0100

    * fix mailer when to is wrong

--- src/Command/RepariereBlocksMitVorgangerCommand.php
+++ src/Command/RepariereBlocksMitVorgangerCommand.php
@@ -2,6 +2,10 @@
 
 namespace App\Command;
 
+use App\Entity\Zeitblock;
+use App\Repository\ActiveRepository;
+use App\Repository\ZeitblockRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -16,25 +20,58 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 )]
 class RepariereBlocksMitVorgangerCommand extends Command
 {
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private ZeitblockRepository    $zeitblockRepository,
+        private ActiveRepository       $activeRepository,
+        string                         $name = null
+    )
+    {
+        parent::__construct($name);
+    }
+
     protected function configure(): void
     {
         $this
-            ->addArgument('blocks', InputArgument::REQUIRED, 'Add Blocks in array notation [id1,id2,....]')
-        ;
+            ->addArgument('schuljahr', InputArgument::REQUIRED, 'Schuljahr to secure this command')
+            ->addArgument('blocks', InputArgument::REQUIRED, 'Add Blocks in array notation [id1,id2,....]');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $counter = 0;
         $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('blocks');
+        $blocks = json_decode($input->getArgument('blocks'));
+        $schuljahr = $this->activeRepository->find($input->getArgument('schuljahr'));
+        foreach ($blocks as $data) {
+            $block = $this->zeitblockRepository->findOneBy(['id' => $data, 'active' => $schuljahr]);
+            if ($block) {
+                $io->info(sprintf('We work with block Nr.: %s', $block->getId()));
+                foreach ($block->getVorganger() as $cBlock){
+                    foreach ($block->getKind() as $child){
+                        if (!in_array($cBlock,$child->getZeitblocks()->toArray())){
+                            $child->addZeitblock($cBlock);
+                            $io->info(sprintf('Child %s %s added  to Block: %s', $child->getVorname(), $child->getNachname(), $cBlock->getId()));
+                            $counter++;
+                        }
 
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
+                        $this->entityManager->persist($child);
+                    }
+                    foreach ($block->getKinderBeworben() as $child){
+                        $io->info(sprintf('We check Child: %s %s', $child->getVorname(), $child->getNachname()));
+                        if (!in_array($cBlock,$child->getBeworben()->toArray())){
+                            $child->addBeworben($cBlock);
+                            $io->info(sprintf('Child %s %s added to BEWORBEN Block %s', $child->getVorname(), $child->getNachname(), $cBlock->getId()));
+                        }
+                        $this->entityManager->persist($child);
+                        $counter++;
+                    }
+                }
+            }
         }
+        $this->entityManager->flush();
 
-
-
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+        $io->success(sprintf('We added %s Child to Blocks', $counter));
 
         return Command::SUCCESS;
     }
