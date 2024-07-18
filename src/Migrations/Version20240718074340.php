21178e4847810466f08a9bcb431a2221dfdb02c3
Entwicklung <entwicklung@h2-invent.com>
2024-07-18 12:52:36 +0200

    remove additional details from cildren

---
+++ src/Migrations/Version20240718074340.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240718074340 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE stadt ADD hide_child_questions TINYINT(1) DEFAULT NULL');
+        $this->addSql('ALTER TABLE zeitblock CHANGE preise preise LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE stadt DROP hide_child_questions');
+        $this->addSql('ALTER TABLE zeitblock CHANGE preise preise LONGTEXT NOT NULL COLLATE `utf8mb4_bin`');
+    }
+}
