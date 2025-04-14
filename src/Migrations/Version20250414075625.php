5ed0e6f9ac284eeaa7f8fec1e0bcef5045399602
Entwicklung <entwicklung@h2-invent.com>
2025-04-14 14:26:02 +0200

    * add posibilty to edit stammdaten from kontingent * remove question from child form

---
+++ src/Migrations/Version20250414075625.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250414075625 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE stadt ADD settings_skib_show_sonnencreme_kinder TINYINT(1) DEFAULT NULL, ADD settings_skib_show_zecken_kinder TINYINT(1) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE stadt DROP settings_skib_show_sonnencreme_kinder, DROP settings_skib_show_zecken_kinder');
+    }
+}
