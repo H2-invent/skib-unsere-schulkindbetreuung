0971e7b37f6069d537499365146d9e2dc457e88f
Entwicklung <entwicklung@h2-invent.com>
2025-06-23 11:35:43 +0200

    fix berehcnugsservice

--- src/Controller/SendTestEmailForTemplateController.php
+++ src/Controller/SendTestEmailForTemplateController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Kind;
 use App\Entity\Organisation;
 use App\Entity\Stammdaten;
+use App\Entity\Zeitblock;
 use App\Repository\StadtRepository;
 use App\Service\MailerService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -49,6 +50,29 @@ class SendTestEmailForTemplateController extends AbstractController
         $kind->setEltern($eltern);
         $organisation = new Organisation();
         $organisation->setName('test ORganisation');
+        $blockBeworben = new Zeitblock();
+        $blockBeworben->setVon((new \DateTimeImmutable())->setTime(7,15));
+        $blockBeworben->setBis((new \DateTimeImmutable())->setTime(9,15));
+        $blockBeworben->setWochentag(1);
+        $blockBeworben->setGanztag(1);
+        $blockBeworben->setPreise([1,2,3,4]);
+        $kind->addBeworben($blockBeworben);
+        $blockInBEtreuung = new Zeitblock();
+        $blockInBEtreuung->setVon((new \DateTimeImmutable())->setTime(10,15));
+        $blockInBEtreuung->setBis((new \DateTimeImmutable())->setTime(12,15));
+        $blockInBEtreuung->setWochentag(1);
+        $blockInBEtreuung->setGanztag(1);
+        $blockInBEtreuung->setPreise([1,2,3,4]);
+        $kind->addZeitblock($blockInBEtreuung);
+        $blockwarteliste = new Zeitblock();
+        $blockwarteliste->setVon((new \DateTimeImmutable())->setTime(10,15));
+        $blockwarteliste->setBis((new \DateTimeImmutable())->setTime(12,15));
+        $blockwarteliste->setWochentag(1);
+        $blockwarteliste->setGanztag(1);
+        $blockwarteliste->setPreise([1,2,3,4]);
+        $kind->addWarteliste($blockwarteliste);
+
+
         // Dummy-Kind zu Eltern hinzufügen (falls Getter+Collection vorhanden)
         if (method_exists($eltern, 'addKind')) {
             $eltern->addKind($kind);
